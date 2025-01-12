8dbc6de21d73c6272ee4c9d1e066425ec2aab649
MGravitee <mgravitee@gmail.com>
2025-01-12 14:07:14 -0800

    Have UX project data coming in to populate the modals, just need to do styling now.

---
+++ src/components/About copy.tsx
@@ -0,0 +1,142 @@
+import { useState, useEffect } from "react";
+import LoadingSpinner from "./LoadingSpinner";
+import { aboutLink } from "../toolbelt/api";
+import { motion } from "framer-motion";
+
+// Define types for API data
+interface AboutData {
+    acf: {
+        about_content_1: string;
+        about_content_2: string;
+        things_i_enjoy: ThingsIEnjoy[];
+    };
+}
+
+interface ThingsIEnjoy {
+    content: string;
+    image: string;
+    title: string;
+}
+
+function About() {
+    const [restData, setData] = useState<AboutData | null>(null);
+    const [isLoaded, setLoadStatus] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(aboutLink);
+            if (response.ok) {
+                const data = await response.json();
+                setData(data);
+                setLoadStatus(true);
+            } else {
+                setLoadStatus(false);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            {isLoaded && restData ? (
+                <>
+                    <section
+                        id="about-section"
+                        className="about-section mb-[8rem] single-section transition-colors shadow-medium rounded-md p-4 bg-content1 backdrop-opacity-50"
+                    >
+                        <h2
+                            className="text-2xl md:text-3xl xl:text-4xl font-medium my-8">
+                            Who Dis?
+                        </h2>
+                        <div>
+                            <p className="mb-8">
+                                {restData.acf.about_content_1}
+                            </p>
+                            <article
+                                className="dangerouslySetHtmlContent max-w-[37.5rem] flex flex-col justify-center"
+                                dangerouslySetInnerHTML={{
+                                    __html:
+                                        restData.acf.about_content_2 ||
+                                        "Coming Soon...",
+                                }}
+                            />
+                        </div>
+                        <aside className="mt-16">
+                            <h2 className="text-lg lg:text-3xl font-medium mb-8">
+                                Some Things I Enjoy:{" "}
+                            </h2>
+                            <ScrollingThings
+                                things={restData.acf.things_i_enjoy}
+                            />
+                        </aside>
+                    </section>
+                </>
+            ) : (
+                <LoadingSpinner />
+            )}
+        </>
+    );
+}
+
+// scrolling things for slightly more interesting about section so it isn't just TLDR
+
+const ScrollingThings = ({ things }: { things: ThingsIEnjoy[] }) => {
+    return (
+        <div className="p-4 overflow-x-hidden relative">
+            <div className="absolute top-0 bottom-0 left-0 w-24 z-10 rounded" />
+            <div className="flex items-center mb-4">
+                {/* passing props to things to ThingsList */}
+                <ThingsList list={things} duration={125} />
+                <ThingsList list={things} duration={125} />
+                <ThingsList list={things} duration={125} />
+                <ThingsList list={things} duration={125} />
+                <ThingsList list={things} duration={125} />
+            </div>
+            <div className="absolute top-0 bottom-0 right-0 w-24 z-10 rounded" />
+        </div>
+    );
+};
+
+// im getting better at typing and making interfaces in typescript, thank god
+const ThingsList = ({
+    list,
+    reverse = false,
+    duration = 50,
+}: {
+    list: ThingsIEnjoy[]; // Update to use ThingsIEnjoy[]
+    reverse?: boolean;
+    duration?: number;
+}) => {
+    return (
+        <motion.div
+            initial={{ translateX: reverse ? "-100%" : "0%" }}
+            animate={{ translateX: reverse ? "0%" : "-100%" }}
+            transition={{ duration, repeat: Infinity, ease: "linear" }}
+            className="flex gap-4 px-2 border-content1-foreground"
+        >
+            {list.map((t, index) => {
+                return (
+                    <article
+                        key={index} // Use index as key
+                        className="shrink-0 w-[500px] grid grid-cols-[10rem,_1fr] rounded-lg overflow-hidden relative"
+                    >
+                        <img
+                            src={t.image}
+                            className="w-full h-full object-cover"
+                        />
+                        <div className="bg-content2 text-current shadow-large p-4 border-content2">
+                            <span className="block font-semibold text-lg lg:text-2xl mb-1">
+                                <h3>{t.title}</h3>
+                            </span>
+                            <span className="block text-sm lg:text-medium text-current">
+                                <p>{t.content}</p>
+                            </span>
+                        </div>
+                    </article>
+                );
+            })}
+        </motion.div>
+    );
+};
+
+export default About;
--- src/components/UXModal.tsx
+++ src/components/UXModal.tsx
@@ -13,174 +13,161 @@ import LoadingSpinner from "./LoadingSpinner";
 import { motion } from "framer-motion";
 
 interface UXData {
+    id: string;
+    acf: {
         title: string;
-        icon: string;
+        icon: {
+            url: string;
+        };
         video: string;
-        content: string;
-    }
-
-
+        content_1: string;
+    };
+}
 
 export default function UXModal() {
     const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [restData, setData] = useState<UXData[] | null>(null);
+    const [isLoaded, setLoadStatus] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(uxLink);
+            if (response.ok) {
+                const data = await response.json();
+                setData(data);
+                setLoadStatus(true);
+            } else {
+                setLoadStatus(false);
+            }
+        };
+        fetchData();
+    }, [uxLink]);
+
+    console.log({ restData });
 
     return (
         <>
-            <Button onPress={onOpen}>Open Modal</Button>
-            <Modal
-                backdrop="opaque"
-                isOpen={isOpen}
-                motionProps={{
-                    variants: {
-                        enter: {
-                            y: 0,
-                            opacity: 1,
-                            transition: {
-                                duration: 0.5,
-                                ease: "easeOut",
-                            },
-                        },
-                        exit: {
-                            y: -20,
-                            opacity: 0,
-                            transition: {
-                                duration: 0.4,
-                                ease: "easeIn",
-                            },
-                        },
-                    },
-                }}
-                size={"5xl"}
-                scrollBehavior={"inside"}
-                onOpenChange={onOpenChange}
-                classNames={{
-                    backdrop:
-                        "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20",
-                }}
-            >
-                <ModalContent>
-                    {(onClose) => (
-                        <>
-                            <ModalHeader className="flex flex-col gap-1">
-                                Modal Title
-                            </ModalHeader>
-                            <ModalBody>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit. Nullam pulvinar risus non
-                                    risus hendrerit venenatis. Pellentesque sit
-                                    amet hendrerit risus, sed porttitor quam.
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit. Nullam pulvinar risus non
-                                    risus hendrerit venenatis. Pellentesque sit
-                                    amet hendrerit risus, sed porttitor quam.
-                                </p>
-                                <p>
-                                    Magna exercitation reprehenderit magna aute
-                                    tempor cupidatat consequat elit dolor
-                                    adipisicing. Mollit dolor eiusmod sunt ex
-                                    incididunt cillum quis. Velit duis sit
-                                    officia eiusmod Lorem aliqua enim laboris do
-                                    dolor eiusmod. Et mollit incididunt nisi
-                                    consectetur esse laborum eiusmod pariatur
-                                    proident Lorem eiusmod et. Culpa deserunt
-                                    nostrud ad veniam.
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit. Nullam pulvinar risus non
-                                    risus hendrerit venenatis. Pellentesque sit
-                                    amet hendrerit risus, sed porttitor quam.
-                                    Magna exercitation reprehenderit magna aute
-                                    tempor cupidatat consequat elit dolor
-                                    adipisicing. Mollit dolor eiusmod sunt ex
-                                    incididunt cillum quis. Velit duis sit
-                                    officia eiusmod Lorem aliqua enim laboris do
-                                    dolor eiusmod. Et mollit incididunt nisi
-                                    consectetur esse laborum eiusmod pariatur
-                                    proident Lorem eiusmod et. Culpa deserunt
-                                    nostrud ad veniam.
-                                </p>
-                                <p>
-                                    Mollit dolor eiusmod sunt ex incididunt
-                                    cillum quis. Velit duis sit officia eiusmod
-                                    Lorem aliqua enim laboris do dolor eiusmod.
-                                    Et mollit incididunt nisi consectetur esse
-                                    laborum eiusmod pariatur proident Lorem
-                                    eiusmod et. Culpa deserunt nostrud ad
-                                    veniam. Lorem ipsum dolor sit amet,
-                                    consectetur adipiscing elit. Nullam pulvinar
-                                    risus non risus hendrerit venenatis.
-                                    Pellentesque sit amet hendrerit risus, sed
-                                    porttitor quam. Magna exercitation
-                                    reprehenderit magna aute tempor cupidatat
-                                    consequat elit dolor adipisicing. Mollit
-                                    dolor eiusmod sunt ex incididunt cillum
-                                    quis. Velit duis sit officia eiusmod Lorem
-                                    aliqua enim laboris do dolor eiusmod. Et
-                                    mollit incididunt nisi consectetur esse
-                                    laborum eiusmod pariatur proident Lorem
-                                    eiusmod et. Culpa deserunt nostrud ad
-                                    veniam.
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipiscing elit. Nullam pulvinar risus non
-                                    risus hendrerit venenatis. Pellentesque sit
-                                    amet hendrerit risus, sed porttitor quam.
-                                </p>
-                                <p>
-                                    Magna exercitation reprehenderit magna aute
-                                    tempor cupidatat consequat elit dolor
-                                    adipisicing. Mollit dolor eiusmod sunt ex
-                                    incididunt cillum quis. Velit duis sit
-                                    officia eiusmod Lorem aliqua enim laboris do
-                                    dolor eiusmod. Et mollit incididunt nisi
-                                    consectetur esse laborum eiusmod pariatur
-                                    proident Lorem eiusmod et. Culpa deserunt
-                                    nostrud ad veniam.
-                                </p>
-                                <p>
-                                    Mollit dolor eiusmod sunt ex incididunt
-                                    cillum quis. Velit duis sit officia eiusmod
-                                    Lorem aliqua enim laboris do dolor eiusmod.
-                                    Et mollit incididunt nisi consectetur esse
-                                    laborum eiusmod pariatur proident Lorem
-                                    eiusmod et. Culpa deserunt nostrud ad
-                                    veniam. Lorem ipsum dolor sit amet,
-                                    consectetur adipiscing elit. Nullam pulvinar
-                                    risus non risus hendrerit venenatis.
-                                    Pellentesque sit amet hendrerit risus, sed
-                                    porttitor quam. Magna exercitation
-                                    reprehenderit magna aute tempor cupidatat
-                                    consequat elit dolor adipisicing. Mollit
-                                    dolor eiusmod sunt ex incididunt cillum
-                                    quis. Velit duis sit officia eiusmod Lorem
-                                    aliqua enim laboris do dolor eiusmod. Et
-                                    mollit incididunt nisi consectetur esse
-                                    laborum eiusmod pariatur proident Lorem
-                                    eiusmod et. Culpa deserunt nostrud ad
-                                    veniam.
-                                </p>
-                            </ModalBody>
-                            <ModalFooter>
-                                <Button
-                                    color="danger"
-                                    variant="light"
-                                    onPress={onClose}
+            {isLoaded && restData ? (
+                <>
+                    <section>
+                        {restData.map((UXData, index) => (
+                            <div key={UXData.id} className="modal-item max-w">
+                                <Button onPress={onOpen}>
+                                    {/* icon */}
+                                    <span className="w-10 h-10 flex items-center justify-center">
+                                        <img
+                                            src={UXData.acf.icon.url}
+                                            alt="Project logo/icon"
+                                            className="w-full h-full object-contain"
+                                        />
+                                    </span>
+
+                                    {/* title and tagline */}
+                                    <div>
+                                        <h3 className="block font-bold">
+                                            {UXData.acf.title}
+                                        </h3>
+                                        <p className="block text-sm text-content3-foreground">
+                                            this
+                                        </p>
+                                    </div>
+                                </Button>
+                                <Modal
+                                    backdrop="opaque"
+                                    isOpen={isOpen}
+                                    motionProps={{
+                                        variants: {
+                                            enter: {
+                                                y: 0,
+                                                opacity: 1,
+                                                transition: {
+                                                    duration: 0.5,
+                                                    ease: "easeOut",
+                                                },
+                                            },
+                                            exit: {
+                                                y: -20,
+                                                opacity: 0,
+                                                transition: {
+                                                    duration: 0.4,
+                                                    ease: "easeIn",
+                                                },
+                                            },
+                                        },
+                                    }}
+                                    size={"5xl"}
+                                    scrollBehavior={"inside"}
+                                    onOpenChange={onOpenChange}
+                                    classNames={{
+                                        backdrop:
+                                            "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20",
+                                    }}
                                 >
-                                    Close
-                                </Button>
-                                <Button color="warning" onPress={onClose}>
-                                    View Project
-                                </Button>
-                            </ModalFooter>
-                        </>
-                    )}
-                </ModalContent>
-            </Modal>
+                                    <ModalContent>
+                                        {(onClose) => (
+                                            <>
+                                                <ModalHeader className="flex flex-col gap-1">
+                                                    {UXData.acf.title}
+                                                </ModalHeader>
+                                                <ModalBody>
+                                                    <div className="media-wrapper flex justify-center w-full">
+                                                        <video
+                                                            key={
+                                                                UXData.acf.video
+                                                            }
+                                                            className="w-full h-auto rounded-md aspect-video"
+                                                            autoPlay
+                                                            loop
+                                                            muted
+                                                            preload="auto"
+                                                        >
+                                                            <source
+                                                                src={
+                                                                    UXData.acf
+                                                                        .video
+                                                                }
+                                                                type="video/mp4"
+                                                            />
+                                                            Your browser does
+                                                            not support the
+                                                            video tag.
+                                                        </video>
+                                                    </div>
+                                                    <div
+                                                        dangerouslySetInnerHTML={{
+                                                            __html:
+                                                            UXData.acf
+                                                            .content_1 ||
+                                                                "Coming Soon...",
+                                                        }}
+                                                    />
+                                                </ModalBody>
+                                                <ModalFooter>
+                                                    <Button
+                                                        color="danger"
+                                                        variant="light"
+                                                        onPress={onClose}
+                                                    >
+                                                        Close
+                                                    </Button>
+                                                    <Button
+                                                        color="warning"
+                                                        onPress={onClose}
+                                                    >
+                                                        View Project
+                                                    </Button>
+                                                </ModalFooter>
+                                            </>
+                                        )}
+                                    </ModalContent>
+                                </Modal>
+                            </div>
+                        ))}
+                    </section>
+                </>
+            ) : (
+                <LoadingSpinner />
+            )}
         </>
     );
 }
